8a678081d3351c221001616989716b69190425b2
anonymous <anonymous>
2016-08-06 21:00:58 -0500

    add white space remove tool to remove leading and trying space

--- src/romancalc.c
+++ src/romancalc.c
@@ -99,6 +99,47 @@ void  rnum_str_free(rn_pair_strct_type * rnum_pair){
 	}
 }
 
+//////////////////////////////////////////
+// Stores the trimmed input string into the given output buffer, which must be
+// large enough to store the result.  If it is too small, the output is
+// truncated.
+// borrowed code
+// http://stackoverflow.com/questions/122616/how-do-i-trim-leading-trailing-whitespace-in-a-standard-way
+// modified fromoriginal code
+size_t trimwhitespace(char *out, size_t len, const char *str)
+{
+	
+	if(len == 0)
+		return 0;
+	
+	const char *end;
+	size_t out_size;
+	
+	// Trim leading space
+	while(isspace(*str)) str++;
+	
+	if(*str == 0)  // All spaces?
+	{
+		*out = 0;
+		return 1;
+	}
+	
+	// Trim trailing space
+	end = str + strlen(str) - 1;
+	while(end > str && isspace(*end)) end--;
+	end++;
+	
+	// Set output size to minimum of trimmed string length and buffer size minus 1
+	out_size = (end - str) < len-1 ? (end - str) : len-1;
+	
+	// Copy trimmed string and add null terminator
+	memcpy(out, str, out_size);
+	out[out_size] = 0;
+	
+	return out_size;
+}
+//////////////////////////////////////////
+
 /*******************************************************
  * rnum_check check digits to ensure they are only roman
  * numerals.
